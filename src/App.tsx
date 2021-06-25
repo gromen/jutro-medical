97413fe9ab613d9d900507a196ba446dda2b4b9d
Marcin Gromek <gromson101@gmail.com>
2021-06-25 13:39:40 +0200

    refactor: Remove unused import entry

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 import Continent from './components/Continent';
